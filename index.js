8658477741901558ab338c90738b9108a99b19d1
jannick <jannicktrollenielsen@hotmail.com>
2022-06-09 23:43:36 +0200

    structure update

--- index.js
+++ index.js
@@ -11,33 +11,43 @@ const client = new Discord.Client({
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
+    client.user.setStatus('online')
+    client.user.setActivity('Hi, im in Alpha!', { 
+        type: 'WATCHING' 
+    })
 })
 
 client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello World!")
     }
-})
-
-client.on("messageCreate", (message) => {
     if (message.content == "lolerswager.com"){
         message.reply("https://www.lolerswager.com/")
     }
-})
-
-const UserArr = "218717194421403648"
-
-client.on("messageCreate", (message) => {
-    if (message.author.id != UserArr){
-        if (message.content.includes("https://discord.com/invite/")){
+    const UserArr = ["218717194421403648"]
+    if (message.author.id != UserArr[0]){
+        if (message.content.includes('https://discord.com/invite/') || message.content.includes('https://discord.gg/')){
             message.delete()
             .then(message => console.log(`${message.author.username} You dont have Permission to Send Discord invites ${message}`))
-            .then(message.guild.channels.cache.get(message.channelId).send(`${message.author.username} You dont have Permission to Send Discord invites`))
+            .then(message.guild.channels.cache.get(message.channelId).send(`<@${message.author.username}> You dont have Permission to Send Discord invites`))
             .catch(console.error)
         }
     }else{
-        message.react('✅')
+        if (message.content.includes('https://discord.com/invite/') || message.content.includes('https://discord.gg/')){
+            message.react('✅')
+        }
     }
+})
+
+// msg when joing discord and leaving
+const welcomeChannelId = "465225542140952624"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+})
+
+client.on("guildMemberRemove", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Bye`)
 })
 
 client.login(process.env.TOKEN)
