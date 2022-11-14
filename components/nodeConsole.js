43831e713f3d4741af8de24e80a36590f6309e79
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2022-11-14 10:30:08 +0100

    update

--- components/nodeConsole.js
+++ components/nodeConsole.js
@@ -2,31 +2,32 @@ import timeDate from "./cal_time.js"
 /* import nodeLogger from "./nodeLogger.js"
 import fs from "fs" */
 
-export default function nodeConsole(consoleColor, subject, text1){
+export default function nodeConsole(consoleColor, subject, text1) {
+    /*  nodeLogger('ConsoleLog', `[${timeDate()}] [${subject.toUpperCase()}] - ${text1}`); */
 
- /*  nodeLogger('ConsoleLog', `[${timeDate()}] [${subject.toUpperCase()}] - ${text1}`); */
-
-    switch(consoleColor) {
+    switch (consoleColor) {
         case "ok":
             consoleColor = "\x1b[32m"
-          break;
+            break
         case "err":
             consoleColor = "\x1b[31m"
-          break;
-          case "error":
+            break
+        case "error":
             consoleColor = "\x1b[31m"
-          break;
+            break
         case "info":
             consoleColor = "\x1b[36m"
-          break;
-          case "warn":
+            break
+        case "warn":
             consoleColor = "\x1b[33m"
-          break;
-          case "warning":
+            break
+        case "warning":
             consoleColor = "\x1b[33m"
-          break;
+            break
         default:
             consoleColor = "\x1b[38m"
-      }
-    return(console.log(`${consoleColor}[${timeDate()}] [${subject.toUpperCase()}] - ${text1} \x1b[0m`))
+    }
+    return console.log(
+        `${consoleColor}[${timeDate()}] [${subject.toUpperCase()}] - ${text1} \x1b[0m`
+    )
 }
